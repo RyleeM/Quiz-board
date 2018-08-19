f44825010183392d50f003844f5fa19e12b695a6
Rylee Muniu <ryleemuniu3@gmail.com>
2018-08-19 15:59:12 +0300

    Submit button added

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 var quizContainer = document.getElementById('quiz');
 var resultsContainer = document.getElementById('results');
 var submitButton = document.getElementById('submit');
+
+function buildQuiz(){
+  }
+
+function displayResults(){
+  }
+
+submitButton.onclick("clickButton"){
+  
+}
