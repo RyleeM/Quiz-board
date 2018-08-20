4a7a373c4d1fc4ea2af79e66a19ccabea5dc5efb
Rylee Muniu <ryleemuniu3@gmail.com>
2018-08-20 16:52:35 +0300

    html file altered

--- js/script.js
+++ js/script.js
@@ -1,46 +1,24 @@
-$function check({
-  var q1=document.myform.q1.value;
-  var q2=document.myform.q2.value;
-  var q3=document.myform.q3.value;
-  var q4=document.myform.q4.value;
-  var q5=document.myform.q5.value;
-  var q6=document.myform.q6.value;
-  var q7=document.myform.q7.value;
-  var q8=document.myform.q8.value;
-  var q9=document.myform.q9.value;
-  var q10=document.myform.q10.value;
-  var count=0;
+$(document).ready(function(){
+  $("form#options").submit(function(event){
 
-if(q1=== "b"){
-      count++;
-}
+    var question1= $("input:radio[name=question1]:checked").val();
+    var question2= $("input:radio[name=question2]:checked").val();
+    var question3= $("input:radio[name=question3]:checked").val();
+    var question4= $("input:radio[name=question4]:checked").val();
+    var question5= $("input:radio[name=question5]:checked").val();
+    var result= parseInt(question1)+parseInt(question2)+parseInt(question3)+parseInt(question4)+parseInt(question5);
 
-if(q2=== "a"){
-      count++;
-}
-if(q3=== "b"){
-      count++;
-}
-if(q4=== "a"){
-      count++;
-}
-if(q5=== "c"){
-      count++;
-}
-if(q6=== "d"){
-      count++;
-}
-if(q7=== "b"){
-      count++;
-}
-if(q8=== "a"){
-      count++;
-}
-if(q9=== "b"){
-      count++;
-}
-if(q10=== "c"){
-      count++;
-}
-$("#submit").click(function(){
-alert("Your score  is "+count+")
+      
+      if(isNaN(result)=== true){
+        $("#result").text("Please ensure all questions are answered");
+        $(".quiz").hide();
+        $(".answer").show();
+      }else{
+
+        $("#result").text("Your total score is "+result+"/100");
+        $(".quiz").hide();
+        $(".answer").show();
+      }
+  event.preventDefault();
+  });
+});
